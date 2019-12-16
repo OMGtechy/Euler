7896609174b7e0d62fe4ca11a7e29b0235d6135a
Joshua Gerrard <joshuagerrard@protonmail.com>
2019-12-16 01:35:47 +0100

    Added program to check each solution still works

---
+++ sanity-check.py
@@ -0,0 +1,23 @@
+import subprocess
+
+def check_case(problemNumber, expectedLastLine):
+    output = subprocess.check_output(f"build/Problem{problemNumber}/Problem{problemNumber}")
+    lastLine = str(output, 'utf-8')
+    lastLine = lastLine.strip()
+    # str to force it to a string, strip in case it already was
+    expectedLastLine = str(expectedLastLine).strip()
+    print(f"Problem {problemNumber} ran, last line of output was {lastLine}. Expected {expectedLastLine}. {'Match!' if lastLine == expectedLastLine else 'No match!'}")
+
+check_case(1, 233168)
+check_case(2, 4613732)
+check_case(3, 6857)
+check_case(4, 906609)
+check_case(5, 232792560)
+check_case(6, 25164150)
+check_case(7, 104743)
+check_case(8, 23514624000)
+check_case(9, 31875000)
+check_case(10, 142913828922)
+check_case(11, 70600674)
+check_case(12, 76576500)
+
